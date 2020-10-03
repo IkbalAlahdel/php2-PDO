e160174d094f3a341202f14644bf5866560d2256
IkbalAlahdel <isash90@gmail.com>
2020-10-03 23:21:17 +0300

    uploading files

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'test';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+
+try {	
+$dbConn = new PDO("mysql:host={$databaseHost};dbname={$databaseName}", $databaseUsername, $databasePassword);
+	$dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Setting Error Mode as Exception
+} catch(PDOException $e) {
+	echo $e->getMessage();
+}
+?>
